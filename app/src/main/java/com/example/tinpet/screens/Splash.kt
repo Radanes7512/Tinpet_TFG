f8e1a3e7174295087abda66be1498c2e88003cfd
royvao <royvao@gmail.com>
2023-02-09 16:25:28 +0100

    add permissions options -- uncoming

--- app/src/main/java/com/example/tinpet/screens/Splash.kt
+++ app/src/main/java/com/example/tinpet/screens/Splash.kt
@@ -1,18 +1,15 @@
 package com.example.tinpet.screens
 
-import android.window.SplashScreen
+import android.Manifest
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
@@ -20,18 +17,23 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.tinpet.R
 import com.example.tinpet.graphs.Graph
 import com.example.tinpet.ui.theme.abrilFatface
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
 import kotlinx.coroutines.delay
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun SplashScreen(navController:NavHostController) {
+    val permissionState = rememberPermissionState(permission = Manifest.permission.INTERNET)
+
     LaunchedEffect(key1 = true){
+        permissionState.launchPermissionRequest()
         delay(3000)
         navController.popBackStack()
         navController.navigate(Graph.AUTHENTICATION)

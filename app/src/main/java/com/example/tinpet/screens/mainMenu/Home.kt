8381fd32a46594571559dc1610fd2c46e5713006
royvao <royvao@gmail.com>
2023-03-07 14:00:27 +0100

    UPDATE HOME SCREEN FIXING  ALL THE ERRORS

--- app/src/main/java/com/example/tinpet/screens/mainMenu/Home.kt
+++ app/src/main/java/com/example/tinpet/screens/mainMenu/Home.kt
@@ -4,11 +4,6 @@ import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-<<<<<<< HEAD
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreHoriz
-=======
->>>>>>> 5e378163f5f4b720cff8eed5b5d4db702008e786
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,17 +16,9 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-<<<<<<< HEAD
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tinpet.R
-=======
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.tinpet.R
-import com.example.tinpet.ui.theme.TinPetTheme
->>>>>>> 5e378163f5f4b720cff8eed5b5d4db702008e786
 import com.example.tinpet.ui.theme.abrilFatface
 
 
@@ -46,7 +33,6 @@ fun HomeScreen() {
         R.drawable.default_pet_4,
         R.drawable.default_pet_5
     )
-<<<<<<< HEAD
     // LISTA DE NOMBRE  ( DE MOMENTO ASÍ)
     val names = listOf(
         "Max", "Scooby", "Calcetines", "Brutus", "Duke"
@@ -77,23 +63,7 @@ fun HomeScreen() {
     var disliked by remember { mutableStateOf(false) }
     var showEndBox by remember { mutableStateOf(false) }
 
-=======
-    // LISTA DE NOMBRE Y EDAD ( DE MOMENTO ASÍ)
-    val descriptions = listOf(
-        "Max, 5 años",
-        "Scooby, 1 año",
-        "Calcetines, 3 años",
-        "Brutus, 6 años",
-        "Duke, 4 años"
-    )
 
-    var selectedVariable by remember { mutableStateOf(0) }
-    var currentIndex by remember { mutableStateOf(0) }
-    var liked by remember { mutableStateOf(false) }
-    var disliked by remember { mutableStateOf(false) }
-    var showEndBox by remember { mutableStateOf(false) }
-
->>>>>>> 5e378163f5f4b720cff8eed5b5d4db702008e786
     // LÓGICA PARA QUE CUANDO NO HAYA MÁS IMÁGENES PARA MOSTAR, SE VEA EL BOX DE FIN
     fun onButtonClick(liked: Boolean) {
         if (!liked && !disliked) {
@@ -115,7 +85,6 @@ fun HomeScreen() {
         EndBox()
     } else {
         Box(
-<<<<<<< HEAD
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp, 16.dp, 16.dp, 100.dp)
@@ -238,120 +207,6 @@ fun EndBox() {
             .padding(16.dp)
     ) {
         Image(
-            modifier = Modifier.align(Alignment.Center),
-            painter = painterResource(id = R.drawable.icon_pawprint),
-            contentDescription = null
-        )
-        Column(
-            modifier = Modifier
-                .align(Alignment.Center)
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                style = TextStyle(
-                    shadow = Shadow(
-                        color = Color.DarkGray, offset = Offset(2.0f, 5.0f), blurRadius = 2f
-                    )
-                ), text = "¡Ups!", fontWeight = FontWeight.Bold, fontSize = 50.sp
-            )
-            Text(
-                style = TextStyle(
-                    shadow = Shadow(
-                        color = Color.DarkGray, offset = Offset(2.0f, 5.0f), blurRadius = 2f
-                    )
-                ), text = "No hay más mascotas cerca por el momento", fontWeight = FontWeight.Bold
-=======
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp, 16.dp, 16.dp, 100.dp)
-        ) {
-            val imageRes = images[currentIndex]
-            val text = descriptions[currentIndex]
-
-            Image(
-                painter = painterResource(imageRes),
-                contentDescription = "My Image",
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop
-            )
-
-            Column(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(bottom = 16.dp)
-            ) {
-                Text(
-                    modifier = Modifier.padding(16.dp),
-                    style = TextStyle(
-                        shadow = Shadow(
-                            color = Color.DarkGray,
-                            offset = Offset(2.0f, 5.0f),
-                            blurRadius = 2f
-                        )
-                    ),
-                    text = text,
-                    fontSize = 32.sp,
-                    fontFamily = abrilFatface,
-                    color = MaterialTheme.colors.onBackground
-                )
-
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    Image(
-                        modifier = Modifier
-                            .clickable { }
-                            .size(70.dp)
-                            .pointerInput(Unit) {
-                                detectTapGestures(
-                                    onTap = {
-                                        if (!liked && !disliked) {
-                                            disliked = true
-                                            onButtonClick(false)
-                                        }
-                                    }
-                                )
-                            },
-                        painter = painterResource(id = R.drawable.icon_notlike),
-                        contentDescription = "Dislike Button",
-                        colorFilter = if (disliked) ColorFilter.tint(Color.Red) else null
-                    )
-
-                    Image(
-                        modifier = Modifier
-                            .clickable { }
-                            .size(70.dp)
-                            .pointerInput(Unit) {
-                                detectTapGestures(
-                                    onTap = {
-                                        if (!liked && !disliked) {
-                                            liked = true
-                                            onButtonClick(true)
-                                        }
-                                    }
-                                )
-                            },
-                        painter = painterResource(id = R.drawable.icon_like),
-                        contentDescription = "Like Button",
-                        colorFilter = if (liked) ColorFilter.tint(Color.Green) else null
-                    )
-                }
-            }
-        }
-    }
-    liked = false
-    disliked = false
-}
-@Composable
-fun EndBox() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Image(
             modifier = Modifier
                 .align(Alignment.Center),
             painter = painterResource(id = R.drawable.icon_pawprint),
@@ -385,7 +240,6 @@ fun EndBox() {
                 ),
                 text = "No hay más mascotas cerca por el momento",
                 fontWeight = FontWeight.Bold
->>>>>>> 5e378163f5f4b720cff8eed5b5d4db702008e786
             )
         }
     }
